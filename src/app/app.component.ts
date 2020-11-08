8506ea424efb208eeb8a9a903a5a35dd533df724
mateuszfranke <mateuszfranke@icloud.com>
2020-11-08 20:28:58 +0100

    form controls fixed, alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import validate = WebAssembly.validate;
 
 @Component({
   selector: 'app-root',
@@ -15,21 +16,21 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     this.checkoutFormGroup = new FormGroup({
       contactInformation: new FormGroup({
-        email: new FormControl(),
-        phone: new FormControl()
+        email: new FormControl(null, [Validators.required]),
+        phone: new FormControl(null, [Validators.required])
       }),
       shippingAddress: new FormGroup({
-        fullName: new FormControl(),
-        address: new FormControl(),
-        city: new FormControl(),
-        country: new FormControl(),
-        postalCode: new FormControl(),
-        saveForNextTime: new FormControl()
+        fullName: new FormControl(null, [Validators.required]),
+        address: new FormControl(null, [Validators.required]),
+        city: new FormControl(null, [Validators.required]),
+        country: new FormControl(null, [Validators.required]),
+        postalCode: new FormControl(null, [Validators.required]),
       }),
     });
   }
   onSubmit(): void {
     console.log(this.checkoutFormGroup);
+    alert(!this.checkoutFormGroup.invalid ? 'form is valid' : 'form is invalid!');
   }
 
   changeColor(): void{
