65cba3ad012efc2273cd86db2e4a8cf340314088
mateuszfranke <mateuszfranke@icloud.com>
2020-11-08 10:43:16 +0100

    ts reactive form implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'checkout-page';
+  checkoutFormGroup: FormGroup;
+
+  ngOnInit(): void {
+    this.checkoutFormGroup = new FormGroup({
+      contactInformation: new FormGroup({
+        email: new FormControl(),
+        phone: new FormControl()
+      }),
+      shippingAddress: new FormGroup({
+        fullName: new FormControl(),
+        address: new FormControl(),
+        city: new FormControl(),
+        country: new FormControl(),
+        postalCode: new FormControl(),
+        saveForNextTime: new FormControl()
+      }),
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.checkoutFormGroup);
+  }
 }
